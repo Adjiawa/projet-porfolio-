f12833c11544b7ca132ff73291105e81efcc6c3e
Adjiawa <adji.awa.mbathie@etu.univ-st-etienne.fr>
2022-12-21 13:53:41 +0100

    Initial commit

---
+++ src/Controller/AdjiAwaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdjiAwaController extends AbstractController
+{
+    #[Route('/adji/awa', name: 'app_adji_awa')]
+    public function index(): Response
+    {
+        return $this->render('adji_awa/index.html.twig', [
+            'controller_name' => 'AdjiAwaController',
+        ]);
+    }
+}
